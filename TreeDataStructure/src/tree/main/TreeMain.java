e594b0442736d3dcc19e7bfe5f15de769d296cb5
sandyrock2020 <sandyrock2020@gmail.com>
2020-01-23 21:27:17 +0530

    test

--- TreeDataStructure/src/tree/main/TreeMain.java
+++ TreeDataStructure/src/tree/main/TreeMain.java
@@ -10,6 +10,7 @@ public class TreeMain {
 	// Driver method
 	public static void main(String[] args) {
 		Node root;
+		
 		root = new Node(1);
 		root.left = new Node(2);
 		root.right = new Node(3);
