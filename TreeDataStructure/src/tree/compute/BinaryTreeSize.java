ec4659322cb59eacffb4f1d86ae9a0e0d905143a
sandyrock2020 <sandyrock2020@gmail.com>
2020-04-16 10:22:34 +0530

    pushing 16 march

--- TreeDataStructure/src/tree/compute/BinaryTreeSize.java
+++ TreeDataStructure/src/tree/compute/BinaryTreeSize.java
@@ -6,28 +6,82 @@ import tree.pojo.Node;
 
 public class BinaryTreeSize implements TreeInterface {
 
+	int maxLevel = 0;
+
 	@Override
 	public void operate(Node root, TreeOperationEnum treeOperationEnum) {
 		getSizeOfBinaryTree(root);
 	}
 
+	/**
+	 * @param root
+	 * @return number of node in binary tree
+	 */
 	public int getSizeOfBinaryTree(Node root) {
 		if (root == null)
 			return 0;
 		return 1 + getSizeOfBinaryTree(root.left) + getSizeOfBinaryTree(root.right);
 	}
 
+	/**
+	 * @param root
+	 * @return node with minimum key value
+	 */
 	public int maximumInBinaryTree(Node root) {
 		if (root == null)
 			return Integer.MIN_VALUE;
 		return Math.max(root.key, Math.max(maximumInBinaryTree(root.left), maximumInBinaryTree(root.right)));
 	}
 
+	/**
+	 * @param root
+	 * @return height of the binary tree
+	 */
 	public int getHeight(Node root) {
 		if (root == null)
 			return 0;
-
 		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
 
+	/**
+	 * @param root
+	 * @param k    // print node the given K level
+	 */
+	public void printKlevelNodes(Node root, int k) {
+		if (root == null)
+			return;
+		if (k == 0)
+			System.out.print(root.key + " ");
+		printKlevelNodes(root.left, k - 1);
+		printKlevelNodes(root.right, k - 1);
+	}
+
+	/**
+	 * @param root
+	 * @param level //print leftmost node at every level
+	 */
+	public void printLeftViewTree(Node root, int level) {
+		if (root == null)
+			return;
+		if (level > maxLevel) {
+			System.out.print(root.key + " ");
+			maxLevel = level;
+		}
+		printLeftViewTree(root.left, level + 1);
+		printLeftViewTree(root.right, level + 1);
+
+	}
+
+	public boolean checkChildrenSumProperty(Node root) {
+		if (root == null)
+			return true;
+		if (root.left == null && root.right == null)
+			return true;
+		int sum = 0;
+		if (root.left != null)
+			sum += root.left.key;
+		if (root.right != null)
+			sum += root.right.key;
+		return root.key == sum && checkChildrenSumProperty(root.left) && checkChildrenSumProperty(root.right);
 	}
 }
--- TreeDataStructure/src/tree/main/TreeMain.java
+++ TreeDataStructure/src/tree/main/TreeMain.java
@@ -10,14 +10,14 @@ public class TreeMain {
 	// Driver method
 	public static void main(String[] args) {
 		Node root;
-		root = new Node(1);
+		root = new Node(10);
 		root.left = new Node(6);
-		root.right = new Node(3);
-		root.left.left = new Node(74);
-		root.left.left.left = new Node(70);
-		root.left.right = new Node(5);
-		root.right.left = new Node(10);
-		root.right.right = new Node(7);
+		root.right = new Node(4);
+		root.left.left = new Node(6);
+		root.left.left.left = new Node(1);
+		root.left.left.right = new Node(5);
+		root.right.left = new Node(4);
+		//root.right.right = new Node(7);
 		TreeInterface recursive = new RecursiveTraversal();
 		TreeInterface itrative = new ItrativeTraversal();
 		//recursive.operate(root,TreeConstants.PRE_ORDER);
@@ -27,7 +27,10 @@ public class TreeMain {
 		//levelOrder.operate(root, null);
 		
 		BinaryTreeSize binaryTree = new BinaryTreeSize();
-		System.out.println("Height "+binaryTree.getHeight(root));
-		
+		//System.out.println("Height "+binaryTree.getHeight(root));
+		//binaryTree.printKlevelNodes(root, 2);
+		//binaryTree.printLeftViewTree(root, 1);
+		boolean checkChildrenSumProperty = binaryTree.checkChildrenSumProperty(root);
+		System.out.println(checkChildrenSumProperty?"Yes":"No");
 	}
 }
